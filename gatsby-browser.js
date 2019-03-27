842ffbaf4670dbc5b3f6b530ef96eb61dc9d1d89
John C. Munson <johncmunson91@gmail.com>
2019-03-27 15:08:19 -0500

    add console.log to test out google analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,7 @@ ReactGA.initialize('UA-130692445-2')
 
 exports.onRouteUpdate = (state, page, pages) => {
   if (process.env.NODE_ENV === 'production') {
+    console.log('fired')
     ReactGA.pageview(state.location.pathname)
   }
 }
